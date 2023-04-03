347a90fa4ef90932e312c9593c0d4629f63763e0
piyush <piyush.mishra1998secondary@gmail.com>
2023-04-03 09:02:52 +0530

    first commit

---
+++ Test/src/test/Test1.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=1,b=4;
+		int c=a+b;
+		System.out.println("c :" + c);
+	}
+
+}
