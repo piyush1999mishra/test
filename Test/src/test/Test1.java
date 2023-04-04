5ebd2bdcf4142a7cda7060bf63e12ebf0958a947
piyush1999mishra <129825307+piyush1999mishra@users.noreply.github.com>
2023-04-04 12:51:47 +0530

    Update Test1.java

--- Test/src/test/Test1.java
+++ Test/src/test/Test1.java
@@ -8,7 +8,7 @@ public class Test1 {
 		int a=1,b=4;
 		int c=a+b;
 		System.out.println("c :" + c);
-		System.out.println("a :"+ a);
+		System.out.println("a + b: "+ c);
 		System.out.println("b : "+b);
 	}
 
